ea4d0032da6d6e331fe29354aaafe877a81a1da4
iajqs <974384209@qq.com>
2020-11-01 15:23:03 +0800

    2020.11.1 设置BERT常量参数

--- SimpleQA/BERT/const.py
+++ SimpleQA/BERT/const.py
@@ -2,10 +2,10 @@ import sys
 
 """ 路径 """
 if sys.platform == "win32":
-    trainDir = "D:/SimpleQA/data/atis/train"
-    validDir = "D:/SimpleQA/data/atis/valid"
-    testDir  = "D:/SimpleQA/data/atis/test"
-    modelDir = "D:/SimpleQA/model"
+    trainDir = "C:/SimpleQA/data/atis/train"
+    validDir = "C:/SimpleQA/data/atis/valid"
+    testDir  = "C:/SimpleQA/data/atis/test"
+    modelDir = "C:/SimpleQA/model"
 
 else:
     trainDir = "/home/cks/program/SimpleQA/data/atis/train"
@@ -19,7 +19,8 @@ COUNTWSIGN    =    3         # 词典特殊字符的数量
 COUNTSSIGN    =    2         # 词槽特殊字符的数量        // SONLY_SIGN 不计入
 COUNTISIGN    =    1         # 意图特殊字符的数量
 WPAD_SIGN     =    0         # pad 在词典的编号
-WUNK_SIGN     =    1         # 未知词在词典的标号
+WBUNK_SIGN    =  100         # 未知词在BERT词典的标号
+WDUNK_SIGN    =    1         # 未知词在DATA词典的标号
 WEOS_SIGN     =    2         # 结束符标号
 SUNK_SIGN     =    0         # 未知词槽标签编号
 SPAD_SIGN     =    1         # pad 在词槽的编号
@@ -27,18 +28,19 @@ IUNK_SIGN     =    0         # 未知意图标签编号
 
 
 """ train.py """
-TRAINITER     =   20         # 迭代训练次数#
+TRAINITER     =  100         # 迭代训练次数#
 BATCHSIZE     =   64         # 切分出来的每个数据块的大小
 WORDSIZE      =    0         # 词典大小
 SLOTSIZE      =    0         # 词槽字典大小
 INTENTSIZE    =    0         # 意图字典大小
-EMBEDDSIZE    =  64          # 词向量大小     // 200
-LSTMHIDSIZE   =  384          # LSTM隐含层大小 // 300
+BERTSIZE      =  768         # BERT编码层大小     // 768
+ENCODERSIZE   =  768         # Encoder层输出大小  // 768
+INTENTHIDSIZE =  768         # Intent层输入大小
 NLAYER        =    1         # LSTM的层数
 DROPOUT       =  0.2         # dropout系数
 MAXLEN        =   21         # 序列最大长度（训练时）
-CLIP          =  100         # 最大梯度值
-LEARNINGRATE  = 1e-1         # 学习速率
+CLIP          =  0.1         # 最大梯度值
+LEARNINGRATE  = 1e-3         # 学习速率
+ADDLENGTH     =    1         # 每个输入序列后追加的有效字符数量   // 一般追加结束符一个有效字符
 BIDIRECTIONAL = True         # 双向LSTM开关
 MULTI_HIDDEN  =    2         # 双向LSTM对应隐藏层*2
-ADDLENGTH     =    1         # 每个输入序列后追加的有效字符数量   // 一般追加结束符一个有效字符

2020.11.1 目前的最优模型 ('acc_intent:', 95.85666293393056, 'slot, precision=95.361190, recall=94.991182, f1=95.175826', 'semantic error(intent, slots are all correct): %f', 84.32250839865621)

SimpleQA/crf/model_test.py
@@ -55,7 +55,7 @@ def evaluateLoss(model, dicts, dataDir):
         for i, batch in enumerate(validIterator):
             MAXLEN, lLensSeqin, BatchSeqIn, BatchSeqOut, BatchLabel = processBatch(batch)
 
-            outputs     = model(BatchSeqIn, lLensSeqin)
+            outputs, slot_crf = model(BatchSeqIn, BatchSeqOut, lLensSeqin)
             outputLabel = outputs[0]
             outputSlots = outputs[1]
 
@@ -97,41 +97,20 @@ def evaluateAccuracy(model, dicts, dataDir):
         for i, batch in enumerate(validIterator):
             MAXLEN, lLensSeqin, BatchSeqIn, BatchSeqOut, BatchLabel = processBatch(batch)
 
-            outputs     = model(BatchSeqIn, lLensSeqin)
+            outputs, slot_crf = model(BatchSeqIn, BatchSeqOut, lLensSeqin)
             outputLabel = outputs[0]
             outputSlots = outputs[1]
 
             """ 正确率计算 """
             _, predictLabel = torch.max(outputLabel, 1)
             _, predictSlot  = torch.max(outputSlots, 2)
+            predictSlot     = model.seq2Slots.crf.CRF.decode(outputSlots)
 
             for index in range(len(batch[0])):
                 correct_intents.append(batch[2][index])
                 pred_intents.append(predictLabel.data.tolist()[index])
-                pred_slots.append([dictSlot[1][str(item)] for item in predictSlot.data.tolist()[index][:lLensSeqin[index] - 1]])
+                pred_slots.append([dictSlot[1][str(item)] for item in predictSlot[index][:lLensSeqin[index] - 1]])
                 correct_slots.append([dictSlot[1][str(item)] for item in batch[1][index][:lLensSeqin[index] - 1]])
-
-                # ''' 意图正确率计算 '''
-                # if batch[2][index] == predictLabel.data.tolist()[index]:
-                #     countLabelAcc += 1
-                # ''' 词槽正确率计算 '''
-                # for index_2 in range(lLensSeqin[index] - 1):
-                #     # print(index_2, batch[1][index][index_2], predictSlot.data.tolist()[index][index_2])
-                #     if batch[1][index][index_2] == predictSlot.data.tolist()[index][index_2]:
-                #         countSlotAcc += 1 / (lLensSeqin[index] - 1)
-
-    # return (countLabelAcc / len(pairsIded), countSlotAcc / len(pairsIded))
-
-    wrongDic = {index: 0 for index in range(len(dictSlot[0]))}
-    for i in range(len(correct_intents)):
-        # print(correct_intents[i], pred_intents[i])
-        if correct_intents[i] != pred_intents[i]:
-            item = correct_intents[i]
-            wrongDic[item] = wrongDic[item] + 1
-    #
-    # print(len(correct_intents))
-    # for item in wrongDic:
-    #     print(item, wrongDic[item])
 
     correct_intents = np.array(correct_intents)
     pred_intents    = np.array(pred_intents)
@@ -142,19 +121,21 @@ def evaluateAccuracy(model, dicts, dataDir):
     return ("acc_intent:", accuracy, "slot, precision=%f, recall=%f, f1=%f" % (precision, recall, f1), "semantic error(intent, slots are all correct): %f", semantic_error)
 
 
+def test():
+    modelLoaded, dicts = load_model(modelDir + "/base", "base.model", "base.json")
 
-modelLoaded, dicts = load_model(modelDir + "/base", "base.model", "base.json")
+    WORDSIZE    = len(dicts[0][0])
+    SLOTSIZE    = len(dicts[1][0])
+    INTENTSIZE  = len(dicts[2][0])
 
-WORDSIZE    = len(dicts[0][0])
-SLOTSIZE    = len(dicts[1][0])
-INTENTSIZE  = len(dicts[2][0])
+    model = initModel(WORDSIZE, SLOTSIZE, INTENTSIZE, isTrain=False)
+    model.load_state_dict(modelLoaded)
+    model.eval()
 
-model = initModel(WORDSIZE, SLOTSIZE, INTENTSIZE, isTrain=False)
-model.load_state_dict(modelLoaded)
-model.eval()
+    # print(WORDSIZE, SLOTSIZE, INTENTSIZE)
 
-print(WORDSIZE, SLOTSIZE, INTENTSIZE)
+    print(evaluateLoss(model, dicts, testDir))      # (0.1551565093395766, 0.08468043408356607)
+    print(evaluateAccuracy(model, dicts, testDir))  # (0.972, 0.95121239)
 
-print(evaluateLoss(model, dicts, validDir))      # (0.1551565093395766, 0.08468043408356607)
-print(evaluateAccuracy(model, dicts, validDir))  # (0.972, 0.95121239)
-
+if __name__ == '__main__':
+    test()

SimpleQA/crf/train.py
@@ -5,11 +5,13 @@ if sys.platform == "win32":
     from SimpleQA.crf.const import *
     from SimpleQA.crf.dataUtil import *
     from SimpleQA.crf.ouptutUtil import *
+    from SimpleQA.crf.model_test import *
 else:
     from .model import *
     from .const import *
     from .dataUtil import *
     from .ouptutUtil import *
+    from .model_test import *
 
 import torch
 import torch.nn as nn
@@ -44,7 +46,7 @@ def initModel(WORDSIZE, SLOTSIZE, INTENTSIZE, isTrain=True):
     seq2Slots     = Seq2Slots(dec_slot=decoderSlot, attn_slot=attnSlot, crf=crf, hidden_size=LSTMHIDSIZE * MULTI_HIDDEN)
 
     model         = Seq2Seq(encoder=encoder, seq2Intent=seq2Intent, seq2Slots=seq2Slots)
-
+    model         = model.cuda() if torch.cuda.is_available() else model
     if isTrain:
         model.apply(init_weights)
     return model
@@ -69,7 +71,7 @@ def train(iter, model=None, optimizer=None, isTrainSlot=True, isTrainIntent=True
     pairsIded       = transIds(pairs, dictWord[0], dictSlot[0], dictIntent[0])  # 将字词都转换为数字id
     # pairsIdedPaded  = pad(pairsIded)                                          # 对数据进行pad填充与长度裁剪
     trainIterator   = splitData(pairsIded)                                      # 讲样例集按BATCHSIZE大小切分成多个块
-
+    trainIterator = trainIterator[:len(trainIterator) - len(trainIterator) // 10]
 
     ''' 设定字典大小参数 '''
     WORDSIZE   = len(dictWord[0])
@@ -87,10 +89,10 @@ def train(iter, model=None, optimizer=None, isTrainSlot=True, isTrainIntent=True
     model.train()                                       # 设定模型状态为训练状态
     epoch_lossIntent = 0                                # 定义总损失
     epoch_lossSlot   = 0
-
+    epoch_lossCRF    = 0
     for epoch, batch in tqdm.tqdm(enumerate(trainIterator)):
         MAXLEN      = getMaxLengthFromBatch(batch, ADDLENGTH)
-        lLensSeqin  = getValidLengthsFromBatch(batch, ADDLENGTH, MAXLEN=MAXLEN)
+        lLensSeqin  = getSeqInLengthsFromBatch(batch, ADDLENGTH, MAXLEN=MAXLEN)
         batch       = padBatch(batch, ADDLENGTH, MAXLEN_TEMP=MAXLEN)   # 按照一个batch一个batch的进行pad
         BatchSeqIn  = batch[0]          # 文本序列
         BatchSeqOut = batch[1]          # 词槽标签序列
@@ -112,20 +114,20 @@ def train(iter, model=None, optimizer=None, isTrainSlot=True, isTrainIntent=True
         loss = lossIntent * 0
         loss = loss + lossIntent if isTrainIntent == True else loss
         loss = loss + lossSlot if isTrainSlot == True else loss
-        loss = loss - slot_crf
+        loss = loss - slot_crf / BATCHSIZE
         loss.backward()
 
         torch.nn.utils.clip_grad_norm_(model.parameters(), CLIP)
         optimizer.step()
 
         epoch_lossIntent += lossIntent.item()
-        epoch_lossSlot  += lossSlot.item()
+        epoch_lossSlot   += lossSlot.item()
+        epoch_lossCRF    -= slot_crf.item()
+        # import time
+        # time.sleep(0.4)
+        # print("iter=%d, epoch=%d / %d: MAXLEN = %d; trainLoss = %f、 intentLoss = %f、 slotLoss = %f、crfLoss = %f " % (iter, epoch, len(trainIterator), MAXLEN, loss.item(), lossIntent, lossSlot, -slot_crf))
 
-        import time
-        time.sleep(0.4)
-        # print("iter=%d, epoch=%d / %d: MAXLEN = %d; trainLoss = %f、 intentLoss = %f、 slotLoss = %f " % (iter, epoch, len(trainIterator), MAXLEN, loss.item(), lossIntent, lossSlot))
-
-    return (epoch_lossIntent / len(trainIterator), epoch_lossSlot / len(trainIterator)),  model, optimizer, (dictWord, dictSlot, dictIntent)
+    return (epoch_lossIntent / len(trainIterator), epoch_lossSlot / len(trainIterator), epoch_lossCRF / len(trainIterator)),  model, optimizer, (dictWord, dictSlot, dictIntent)
 
 def evaluate(model, dicts):
 
@@ -149,19 +151,19 @@ def evaluate(model, dicts):
     ''' 模型验证 '''
     model.eval()
     epoch_lossIntent = 0
-    epoch_lossSlot  = 0
-
+    epoch_lossSlot   = 0
+    epoch_lossCRF    = 0
     with torch.no_grad():
         for i, batch in enumerate(validIterator):
             MAXLEN      = getMaxLengthFromBatch(batch, ADDLENGTH)
-            lLensSeqin  = getValidLengthsFromBatch(batch, ADDLENGTH, MAXLEN=MAXLEN)
+            lLensSeqin  = getSeqInLengthsFromBatch(batch, ADDLENGTH, MAXLEN=MAXLEN)
             batch       = padBatch(batch, ADDLENGTH, MAXLEN_TEMP=MAXLEN)  # 按照一个batch一个batch的进行pad
             BatchSeqIn  = batch[0]  # 文本序列
             BatchSeqOut = batch[1]  # 词槽标签序列
             BatchIntent = batch[2]  # 意图标签
             BatchSeqIn, BatchSeqOut, BatchIntent = vector2Tensor(BatchSeqIn, BatchSeqOut, BatchIntent)
 
-            outputs      = model(BatchSeqIn, lLensSeqin)
+            outputs, slot_crf = model(seqIn=BatchSeqIn, seqOut=BatchSeqOut, lLensSeqin=lLensSeqin)
             outputIntent = outputs[0]
             outputSlots  = outputs[1]
 
@@ -173,7 +175,8 @@ def evaluate(model, dicts):
 
             epoch_lossIntent += lossIntent.item()
             epoch_lossSlot   += lossSlot.item()
-    return (epoch_lossIntent / len(validIterator), epoch_lossSlot / len(validIterator))
+            epoch_lossCRF    -= slot_crf.item()
+    return (epoch_lossIntent / len(validIterator), epoch_lossSlot / len(validIterator), epoch_lossCRF / len(validIterator))
 
 
 if __name__ == '__main__':
@@ -186,12 +189,16 @@ if __name__ == '__main__':
         trainLoss, model, optimizer, dicts = train(iter, model=model, optimizer=optimizer, isTrainIntent=True, isTrainSlot=True)
 
         validLoss = evaluate(model, dicts)
-        print("iter %d / %d: trainLoss = (intent=%f, slot=%f), validLoss = (intent=%f, slot=%f)" %
-              (iter, TRAINITER, trainLoss[0], trainLoss[1], validLoss[0], validLoss[1]))
-        if validLoss[0] + validLoss[1] < lossMin:
-            lossMin = validLoss[0] + validLoss[1]
+        print("iter %d / %d: trainLoss = (intent=%f, slot=%f, crf=%f), validLoss = (intent=%f, slot=%f, crf=%f)" %
+              (iter, TRAINITER, trainLoss[0], trainLoss[1], trainLoss[2], validLoss[0], validLoss[1], validLoss[2]))
+
+
+        if validLoss[0] + validLoss[1] + validLoss[2] < lossMin:
+            lossMin = validLoss[0] + validLoss[1] + validLoss[2]
             modelBest = model
             save_model(modelBest, dicts, modelDir + "/base", "base.model", "base.json")
+            # print("update", end='\t')
+            test()
         if trainLoss[0] + trainLoss[1] < 0.1:
             for p in optimizer.param_groups:
                 p['lr'] *= 0.9
@@ -200,6 +207,4 @@ if __name__ == '__main__':
             for p in optimizer.param_groups:
                 p['lr'] = 1e-3
 
-
-    # save_model(modelBest, dicts, modelDir + "/base", "base.model", "base.json")
-
+        print()